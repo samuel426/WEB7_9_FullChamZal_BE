f1912d3ff3e5af9ec12dd2d323707098e8348cf1
aovor <0hy2s2@naver.com>
2025-12-09 17:44:48 +0900

    test(global): 테스트 프로파일 지정

--- src/test/java/back/fcz/FczApplicationTests.java
+++ src/test/java/back/fcz/FczApplicationTests.java
@@ -2,8 +2,10 @@ package back.fcz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FczApplicationTests {
 
     @Test
