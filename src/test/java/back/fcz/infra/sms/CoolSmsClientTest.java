16963b9b5aa320b507fa9217df021c5a2f7ec096
aovor <0hy2s2@naver.com>
2025-12-11 11:11:18 +0900

    fix(test): sms ci 테스트 제외, application-test.yml 수정

--- src/test/java/back/fcz/infra/sms/CoolSmsClientTest.java
+++ src/test/java/back/fcz/infra/sms/CoolSmsClientTest.java
@@ -2,15 +2,17 @@ package back.fcz.infra.sms;
 
 import com.solapi.sdk.message.model.Message;
 import com.solapi.sdk.message.service.DefaultMessageService;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
-import static org.assertj.core.api.Assertions.*;
 
 @ActiveProfiles("test")
+@Tag("sms")
 public class CoolSmsClientTest {
 
     @Test
