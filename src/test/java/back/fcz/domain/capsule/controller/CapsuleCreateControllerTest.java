9d29846446b842862baee1aa1d4bdac81998f214
yhwHUB <yanghw1902@gmail.com>
2025-12-14 16:52:43 +0900

    test(capsule) : 통합 테스트 시 security 필터 제거

--- src/test/java/back/fcz/domain/capsule/controller/CapsuleCreateControllerTest.java
+++ src/test/java/back/fcz/domain/capsule/controller/CapsuleCreateControllerTest.java
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @Transactional
 class CapsuleCreateControllerIntegrationTest {
 
